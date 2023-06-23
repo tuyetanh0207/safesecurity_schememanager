39dac3d5c0baac0fe5aaffbdc970b5822f235ac1
duongitus <duongdeep@gmail.com>
2023-06-24 01:37:36 +0700

    duongitus

--- Project_SS/Project_SS/Forms/Scheme.Designer.cs
+++ Project_SS/Project_SS/Forms/Scheme.Designer.cs
@@ -28,73 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelRight = new System.Windows.Forms.Panel();
-            this.lblTitle = new System.Windows.Forms.Label();
-            this.panelLeft = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panelRight.SuspendLayout();
-            this.panelLeft.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // panelRight
-            // 
-            this.panelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(230)))), ((int)(((byte)(242)))));
-            this.panelRight.Controls.Add(this.lblTitle);
-            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panelRight.Location = new System.Drawing.Point(421, 0);
-            this.panelRight.Name = "panelRight";
-            this.panelRight.Size = new System.Drawing.Size(761, 673);
-            this.panelRight.TabIndex = 3;
-            // 
-            // lblTitle
-            // 
-            this.lblTitle.AutoSize = true;
-            this.lblTitle.Font = new System.Drawing.Font("Malgun Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.ForeColor = System.Drawing.Color.Black;
-            this.lblTitle.Location = new System.Drawing.Point(53, 267);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(643, 124);
-            this.lblTitle.TabIndex = 15;
-            this.lblTitle.Text = "phân công công việc giai đoạn 2 \r\nwinform của TD: bảng dean\r\nbao gồm đầy đủ 4 tha" +
+			this.panelRight = new System.Windows.Forms.Panel();
+			this.lblTitle = new System.Windows.Forms.Label();
+			this.panelLeft = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panelRight.SuspendLayout();
+			this.panelLeft.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panelRight
+			// 
+			this.panelRight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(230)))), ((int)(((byte)(242)))));
+			this.panelRight.Controls.Add(this.lblTitle);
+			this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panelRight.Location = new System.Drawing.Point(421, 0);
+			this.panelRight.Name = "panelRight";
+			this.panelRight.Size = new System.Drawing.Size(761, 673);
+			this.panelRight.TabIndex = 3;
+			// 
+			// lblTitle
+			// 
+			this.lblTitle.AutoSize = true;
+			this.lblTitle.Font = new System.Drawing.Font("Malgun Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblTitle.ForeColor = System.Drawing.Color.Black;
+			this.lblTitle.Location = new System.Drawing.Point(45, 155);
+			this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblTitle.Name = "lblTitle";
+			this.lblTitle.Size = new System.Drawing.Size(643, 124);
+			this.lblTitle.TabIndex = 15;
+			this.lblTitle.Text = "phân công công việc giai đoạn 2 \r\nwinform của TD: bảng dean\r\nbao gồm đầy đủ 4 tha" +
     "o tác delete, update, insert, select \r\ntùy theo từng loại user \r\n";
-            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
-            // 
-            // panelLeft
-            // 
-            this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(121)))), ((int)(((byte)(195)))));
-            this.panelLeft.Controls.Add(this.pictureBox1);
-            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeft.Location = new System.Drawing.Point(0, 0);
-            this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(431, 673);
-            this.panelLeft.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Project_SS.Properties.Resources.LogoText_white__4x;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Scheme
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1182, 673);
-            this.Controls.Add(this.panelRight);
-            this.Controls.Add(this.panelLeft);
-            this.Name = "Scheme";
-            this.Text = "Scheme";
-            this.panelRight.ResumeLayout(false);
-            this.panelRight.PerformLayout();
-            this.panelLeft.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
+			this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+			// 
+			// panelLeft
+			// 
+			this.panelLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(121)))), ((int)(((byte)(195)))));
+			this.panelLeft.Controls.Add(this.pictureBox1);
+			this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panelLeft.Location = new System.Drawing.Point(0, 0);
+			this.panelLeft.Name = "panelLeft";
+			this.panelLeft.Size = new System.Drawing.Size(431, 673);
+			this.panelLeft.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Project_SS.Properties.Resources.LogoText_white__4x;
+			this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Scheme
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1182, 673);
+			this.Controls.Add(this.panelRight);
+			this.Controls.Add(this.panelLeft);
+			this.Name = "Scheme";
+			this.Text = "Scheme";
+			this.panelRight.ResumeLayout(false);
+			this.panelRight.PerformLayout();
+			this.panelLeft.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
